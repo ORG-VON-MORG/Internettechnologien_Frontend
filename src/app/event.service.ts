f8f285ee4e3abbc031b7bf2061b4b0ce50135726
master <orggottschalk@web.de>
2019-04-01 00:17:41 +0200

    Hotfix

--- src/app/event.service.ts
+++ src/app/event.service.ts
@@ -21,35 +21,7 @@ export class EventService {
 
   public getByUserId(userId: number): Observable<Event[]> {
     return new Observable((observer) => {
-      observer.next([
-        {
-          id: 1,
-          title: 'Test Event 1',
-          userId: 1,
-          dateBegin: moment(),
-          dateEnd: moment().add('minutes', 30),
-          priority: EventPriority.LOW,
-          color: '#4D007A'
-        },
-        {
-          id: 2,
-          title: 'Test Event 2',
-          userId: 1,
-          dateBegin: moment().add('minutes', 30),
-          dateEnd: moment().add('hours', 1),
-          priority: EventPriority.MEDIUM,
-          color: '#60B515'
-        },
-        {
-          id: 3,
-          title: 'Test Event 3',
-          userId: 1,
-          dateBegin: moment().add('minutes', 60),
-          dateEnd: moment().add('hours', 2),
-          priority: EventPriority.URGENT,
-          color: '#FF8400'
-        }
-      ]);
+      observer.next([]);
       observer.complete();
     });
     // return this.http.get(`${this.serverUrl}${userId}`);
